e85a94498da8ea74df2651be6dfd895d0a582ee9
Emanuele Pace <emanuele.10@outlook.it>
2020-06-23 22:33:30 +0100

    First Test - DONE

---
+++ note-model-test.js
@@ -0,0 +1,5 @@
+function saveTextTest() {
+    var note = new Note("My favourite language is JavaScript");
+    assert.isTrue(note.text === "My favourite language is JavaScript")
+};
+saveTextTest();
