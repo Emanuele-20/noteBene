d855e5332bb896acba3cd42f65e688397812b8be
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-23 22:51:56 +0100

    Note has a method showText, tests passing

--- note-model-test.js
+++ note-model-test.js
@@ -2,4 +2,10 @@ function saveTextTest() {
     var note = new Note("My favourite language is JavaScript");
     assert.isTrue(note.text === "My favourite language is JavaScript")
 };
+
+function showTextTest() {
+    var note = new Note("text whatever");
+    assert.isTrue(note.showText() === "text whatever");
+}
 saveTextTest();
+showTextTest();
--- note-model.js
+++ note-model.js
@@ -3,5 +3,9 @@
 function Note(text){
     this.text = text;
 };
+
+Note.prototype.showText = function() {
+    return this.text;
+}
     exports.Note = Note;
 })(this);
